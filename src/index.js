c749c1154c60054dea588f95fc564ff32fc21be0
RayHaha <g820324@gmail.com>
2018-11-14 17:49:52 -0800

    the naive way just duplicated it three time in index.js code

--- src/index.js
+++ src/index.js
@@ -21,6 +21,36 @@ const App = () => {
                     <div className="text">Nice blog post!</div>
                 </div>
             </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    {/* use faker library to generate a fake image */}
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    {/* use faker library to generate a fake image */}
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
         </div>
     );
 } 
